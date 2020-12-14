591cbcf926d5a4cb818f1454aee6581720d1473b
Dylan ANTONIOTTI <dylan.antoniotti@gmail.com>
2020-12-14 12:02:37 +0100

    Initial commit
    Initialisation du projet

---
+++ src/test/java/fr/mesi/MESIKABP/MesikabpApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.mesi.MESIKABP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesikabpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
