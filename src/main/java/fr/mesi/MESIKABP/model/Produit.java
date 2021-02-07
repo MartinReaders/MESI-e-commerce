61dee4e0016e2929197c596f98a5bd4f6993ef84
PierrePocheron <pierre.pocheron@gmail.com>
2021-02-07 12:44:04 +0100

    Update Model : Produit

--- src/main/java/fr/mesi/MESIKABP/model/Produit.java
+++ src/main/java/fr/mesi/MESIKABP/model/Produit.java
@@ -7,6 +7,8 @@ import java.util.Set;
 @Entity
 public class Produit {
 
+    // Column of Produit
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "idProduit")
@@ -18,8 +20,14 @@ public class Produit {
     @Column(name = "marque")
     private String marque;
 
+    @Column(name = "modele")
+    private String modele;
+
     @Column(name = "prix")
     private Double prix;
+
+    @Column(name = "image")
+    private String image;
 
     @Column(name = "quantite")
     private Integer quantite;
@@ -27,8 +35,43 @@ public class Produit {
     @Column(name = "type")
     private Integer type;
 
+    @Column(name = "couleur")
+    private String couleur;
+
+    @Column(name = "couleurMarque")
+    private String couleurMarque;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "rgb")
+    private Boolean rgb;
+
     @OneToMany(mappedBy = "produit")
     private Set<LienPanierProduit> paniers;
+
+    // Constructor
+
+    public Produit() {
+    }
+
+    public Produit(Long id, String code, String marque, String modele, Double prix, String image, Integer quantite, Integer type, String couleur, String couleurMarque, String description, Boolean rgb, Set<LienPanierProduit> paniers) {
+        this.id = id;
+        this.code = code;
+        this.marque = marque;
+        this.modele = modele;
+        this.prix = prix;
+        this.image = image;
+        this.quantite = quantite;
+        this.type = type;
+        this.couleur = couleur;
+        this.couleurMarque = couleurMarque;
+        this.description = description;
+        this.rgb = rgb;
+        this.paniers = paniers;
+    }
+
+    // Getter & Setter
 
     public Long getId() {
         return id;
@@ -78,23 +121,79 @@ public class Produit {
         this.type = type;
     }
 
+    public String getModele() {
+        return modele;
+    }
+
+    public void setModele(String modele) {
+        this.modele = modele;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getCouleur() {
+        return couleur;
+    }
+
+    public void setCouleur(String couleur) {
+        this.couleur = couleur;
+    }
+
+    public String getCouleurMarque() {
+        return couleurMarque;
+    }
+
+    public void setCouleurMarque(String couleurMarque) {
+        this.couleurMarque = couleurMarque;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getRgb() {
+        return rgb;
+    }
+
+    public void setRgb(Boolean rgb) {
+        this.rgb = rgb;
+    }
+
+    // Methodes
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof Produit)) return false;
         Produit produit = (Produit) o;
-        return Objects.equals(id, produit.id) &&
-                Objects.equals(code, produit.code) &&
-                Objects.equals(marque, produit.marque) &&
-                Objects.equals(prix, produit.prix) &&
-                Objects.equals(quantite, produit.quantite) &&
-                Objects.equals(type, produit.type) &&
+        return Objects.equals(getId(), produit.getId()) &&
+                Objects.equals(getCode(), produit.getCode()) &&
+                Objects.equals(getMarque(), produit.getMarque()) &&
+                Objects.equals(getModele(), produit.getModele()) &&
+                Objects.equals(getPrix(), produit.getPrix()) &&
+                Objects.equals(getImage(), produit.getImage()) &&
+                Objects.equals(getQuantite(), produit.getQuantite()) &&
+                Objects.equals(getType(), produit.getType()) &&
+                Objects.equals(getCouleur(), produit.getCouleur()) &&
+                Objects.equals(getCouleurMarque(), produit.getCouleurMarque()) &&
+                Objects.equals(getDescription(), produit.getDescription()) &&
+                Objects.equals(getRgb(), produit.getRgb()) &&
                 Objects.equals(paniers, produit.paniers);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, code, marque, prix, quantite, type, paniers);
+        return Objects.hash(getId(), getCode(), getMarque(), getModele(), getPrix(), getImage(), getQuantite(), getType(), getCouleur(), getCouleurMarque(), getDescription(), getRgb(), paniers);
     }
 
     @Override
@@ -103,9 +202,15 @@ public class Produit {
                 "id=" + id +
                 ", code='" + code + '\'' +
                 ", marque='" + marque + '\'' +
+                ", modele='" + modele + '\'' +
                 ", prix=" + prix +
+                ", image='" + image + '\'' +
                 ", quantite=" + quantite +
                 ", type=" + type +
+                ", couleur='" + couleur + '\'' +
+                ", couleurMarque='" + couleurMarque + '\'' +
+                ", description='" + description + '\'' +
+                ", rgb=" + rgb +
                 ", paniers=" + paniers +
                 '}';
     }
