93c8e5fee542ad3b4fa996d2abee4847503b74e3
PierrePocheron <pierre.pocheron@gmail.com>
2021-02-07 12:54:57 +0100

    Update Model : Utilisateur

--- src/main/java/fr/mesi/MESIKABP/model/Utilisateur.java
+++ src/main/java/fr/mesi/MESIKABP/model/Utilisateur.java
@@ -4,12 +4,16 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.text.DateFormat;
 import java.util.Collection;
+import java.util.Date;
 import java.util.Objects;
 
 
 @Entity
 public class Utilisateur implements UserDetails {
+
+    // Column of Utilisateur
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,6 +31,38 @@ public class Utilisateur implements UserDetails {
     @Column(name = "grade")
     private Integer grade;
 
+    @Column(name = "nom")
+    private String nom;
+
+    @Column(name = "prenom")
+    private String prenom;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "telephone")
+    private Integer telephone;
+
+    @Column(name = "dateNaissance")
+    private Date dateNaissance;
+
+    @Column(name = "adresse1")
+    private String adresse1;
+
+    @Column(name = "adresse2")
+    private String adresse2;
+
+    @Column(name = "codePostale")
+    private Integer codePostale;
+
+    @Column(name = "ville")
+    private String ville;
+
+    @Column(name = "pays")
+    private String pays;
+
+    // Constructor
+
     public Utilisateur(){
 
     }
@@ -36,6 +72,25 @@ public class Utilisateur implements UserDetails {
         this.password = password;
         this.grade = grade;
     }
+
+    public Utilisateur(Long id, String login, String password, Integer grade, String nom, String prenom, String email, Integer telephone, Date dateNaissance, String adresse1, String adresse2, Integer codePostale, String ville, String pays) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.grade = grade;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.telephone = telephone;
+        this.dateNaissance = dateNaissance;
+        this.adresse1 = adresse1;
+        this.adresse2 = adresse2;
+        this.codePostale = codePostale;
+        this.ville = ville;
+        this.pays = pays;
+    }
+
+    // Getter & Setter
 
     public Long getId() {
         return id;
@@ -99,22 +154,114 @@ public class Utilisateur implements UserDetails {
         this.grade = grade;
     }
 
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(Integer telephone) {
+        this.telephone = telephone;
+    }
+
+    public Date getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(Date dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public String getAdresse1() {
+        return adresse1;
+    }
+
+    public void setAdresse1(String adresse1) {
+        this.adresse1 = adresse1;
+    }
+
+    public String getAdresse2() {
+        return adresse2;
+    }
+
+    public void setAdresse2(String adresse2) {
+        this.adresse2 = adresse2;
+    }
+
+    public Integer getCodePostale() {
+        return codePostale;
+    }
+
+    public void setCodePostale(Integer codePostale) {
+        this.codePostale = codePostale;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getPays() {
+        return pays;
+    }
+
+    public void setPays(String pays) {
+        this.pays = pays;
+    }
+
+    // Override Methods
+
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof Utilisateur)) return false;
         Utilisateur that = (Utilisateur) o;
-        return Objects.equals(id, that.id) &&
-                Objects.equals(login, that.login) &&
-                Objects.equals(password, that.password) &&
-                Objects.equals(grade, that.grade);
+        return Objects.equals(getId(), that.getId()) &&
+                Objects.equals(getLogin(), that.getLogin()) &&
+                Objects.equals(getPassword(), that.getPassword()) &&
+                Objects.equals(getGrade(), that.getGrade()) &&
+                Objects.equals(getNom(), that.getNom()) &&
+                Objects.equals(getPrenom(), that.getPrenom()) &&
+                Objects.equals(getEmail(), that.getEmail()) &&
+                Objects.equals(getTelephone(), that.getTelephone()) &&
+                Objects.equals(getDateNaissance(), that.getDateNaissance()) &&
+                Objects.equals(getAdresse1(), that.getAdresse1()) &&
+                Objects.equals(getAdresse2(), that.getAdresse2()) &&
+                Objects.equals(getCodePostale(), that.getCodePostale()) &&
+                Objects.equals(getVille(), that.getVille()) &&
+                Objects.equals(getPays(), that.getPays());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, login, password, grade);
+        return Objects.hash(getId(), getLogin(), getPassword(), getGrade(), getNom(), getPrenom(), getEmail(), getTelephone(), getDateNaissance(), getAdresse1(), getAdresse2(), getCodePostale(), getVille(), getPays());
     }
-
 
     @Override
     public String toString() {
@@ -123,8 +270,16 @@ public class Utilisateur implements UserDetails {
                 ", login='" + login + '\'' +
                 ", password='" + password + '\'' +
                 ", grade=" + grade +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", email='" + email + '\'' +
+                ", telephone=" + telephone +
+                ", dateNaissance=" + dateNaissance +
+                ", adresse1='" + adresse1 + '\'' +
+                ", adresse2='" + adresse2 + '\'' +
+                ", codePostale=" + codePostale +
+                ", ville='" + ville + '\'' +
+                ", pays='" + pays + '\'' +
                 '}';
     }
-
-
 }
