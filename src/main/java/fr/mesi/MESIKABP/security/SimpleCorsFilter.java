40f7e39799ae7fc5d1049cafd59ad765adb5d1fc
Dylan ANTONIOTTI <dylan.antoniotti@gmail.com>
2021-02-08 18:34:06 +0100

    Update SimpleCorsFilter.java

--- src/main/java/fr/mesi/MESIKABP/security/SimpleCorsFilter.java
+++ src/main/java/fr/mesi/MESIKABP/security/SimpleCorsFilter.java
@@ -14,9 +14,6 @@ import java.io.IOException;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class SimpleCorsFilter implements Filter {
 
-    @Value("${spring.cors.allowOrigin}")
-    private String allowOrigin;
-
     public SimpleCorsFilter() {
     }
 
@@ -24,7 +21,7 @@ public class SimpleCorsFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
-        response.setHeader("Access-Control-Allow-Origin", allowOrigin);
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "user-agent, host, accept, accept-language, accept-encoding, dnt, content-type, refererer, content-length, connection, x-requested-with, authorization, access-control-allow-origin, cache-control, pragma, withCredentials");
