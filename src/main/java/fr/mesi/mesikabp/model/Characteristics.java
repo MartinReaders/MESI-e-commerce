e23520867bee97588e8e7d82a64a2b27cad84c2e
bouch mana <mana99hotmail.fr>
2021-03-22 14:31:41 +0100

    model characterisics

--- src/main/java/fr/mesi/mesikabp/model/Characteristics.java
+++ src/main/java/fr/mesi/mesikabp/model/Characteristics.java
@@ -1,2 +1,73 @@
-package fr.mesi.mesikabp.model;public class characteristics {
+package fr.mesi.mesikabp.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+public class Characteristics {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idcharacteristics")
+    private Long idcharacteristics;
+
+    @Column(name = "idTypeProduct")
+    private Long idTypeProduct;
+
+    @Column(name = "title")
+    private String title;
+
+    public Characteristics(Long idcharacteristics, Long idTypeProduct, String title) {
+        this.idcharacteristics = idcharacteristics;
+        this.idTypeProduct = idTypeProduct;
+        this.title = title;
+    }
+
+    public Long getIdcharacteristics() {
+        return idcharacteristics;
+    }
+
+    public void setIdcharacteristics(Long idcharacteristics) {
+        this.idcharacteristics = idcharacteristics;
+    }
+
+    public Long getIdTypeProduct() {
+        return idTypeProduct;
+    }
+
+    public void setIdTypeProduct(Long idTypeProduct) {
+        this.idTypeProduct = idTypeProduct;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Characteristics)) return false;
+        Characteristics that = (Characteristics) o;
+        return Objects.equals(idcharacteristics, that.idcharacteristics) && Objects.equals(idTypeProduct, that.idTypeProduct) && Objects.equals(title, that.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idcharacteristics, idTypeProduct, title);
+    }
+
+    @Override
+    public String toString() {
+        return "characteristics{" +
+                "idcharacteristics=" + idcharacteristics +
+                ", idTypeProduct=" + idTypeProduct +
+                ", title='" + title + '\'' +
+                '}';
+    }
 }
