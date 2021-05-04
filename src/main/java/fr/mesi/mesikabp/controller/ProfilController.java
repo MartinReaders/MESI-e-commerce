9dac1c9052d0a5c745a20b4d716dbd4830312adb
Dylan ANTONIOTTI <dylan.antoniotti@gmail.com>
2021-05-04 15:20:18 +0200

    Pass parameter for thymeleaf template

--- src/main/java/fr/mesi/mesikabp/controller/ProfilController.java
+++ src/main/java/fr/mesi/mesikabp/controller/ProfilController.java
@@ -1,15 +1,35 @@
 package fr.mesi.mesikabp.controller;
 
+import fr.mesi.mesikabp.dto.UserDto;
+import fr.mesi.mesikabp.service.AuthService;
+import fr.mesi.mesikabp.service.ModelMapService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class ProfilController {
 
-    @GetMapping("/profile/{id}")
-    public String getProfilPage(final ModelMap model, @PathVariable Long id) {
+    @Autowired
+    private ModelMapService modelMapService;
+
+    @Autowired
+    private AuthService authService;
+
+    @GetMapping("/profile")
+    public String getProfilPage(HttpServletRequest request, final ModelMap model) {
+        UserDto userDto = (UserDto) request.getSession().getAttribute("user");
+        if(userDto == null) {
+            //Retourner 402
+            return "";
+        }
+
+        userDto = modelMapService.convertToDto(authService.getUserInfoByLogin(userDto.getLogin()));
+        model.put("user", userDto);
         return "profile";
     }
 
