d82a10b775ed7f2ccdff9e15a07c814029e8aae4
MartinReaders <martin.khanpherian@gmail.com>
2021-05-30 19:47:30 +0200

    les listes

---
+++ src/main/java/fr/mesi/mesikabp/repository/BrandRepository.java
@@ -0,0 +1,8 @@
+package fr.mesi.mesikabp.repository;
+
+import fr.mesi.mesikabp.model.Brand;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BrandRepository  extends JpaRepository<Brand, Long> {
+}
